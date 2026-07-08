778c060950ea543a8d1d549b18842b9577b3386d
zhenhuaxi <qlc001@qilincl.top>
2026-07-08 15:02:34 +0800

    feat: add E2E skeleton for BookDetail (Issue #496)
    Closes #496

---
+++ packages/studio/e2e/book-detail-496.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+// E2E skeleton: BookDetail
+// Issue #496
+// TODO: fill real E2E code in review/test task
+
+test.describe("BookDetail E2E", () => {
+  test("1. Info display", async ({ page }) => {
+    test.skip();
+  })
+
+  test("2. Edit and save", async ({ page }) => {
+    test.skip();
+  })
+
+  test("3. Delete flow", async ({ page }) => {
+    test.skip();
+  })
+});
