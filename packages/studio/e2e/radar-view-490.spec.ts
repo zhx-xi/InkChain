668f4275a8e4c98b7f459079b0678cee36aa3417
zhenhuaxi <qlc001@qilincl.top>
2026-07-08 15:02:10 +0800

    feat: add E2E skeleton for RadarView (Issue #490)
    Closes #490

---
+++ packages/studio/e2e/radar-view-490.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+// E2E skeleton: RadarView
+// Issue #490
+// TODO: fill real E2E code in review/test task
+
+test.describe("RadarView E2E", () => {
+  test("1. Page renders", async ({ page }) => {
+    test.skip();
+  })
+
+  test("2. Data display", async ({ page }) => {
+    test.skip();
+  })
+
+  test("3. Refresh action", async ({ page }) => {
+    test.skip();
+  })
+});
