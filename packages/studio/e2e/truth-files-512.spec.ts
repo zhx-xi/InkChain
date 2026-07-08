1b63c869292a4073237ac39846433a4f2608bba0
zhenhuaxi <qlc001@qilincl.top>
2026-07-08 15:03:43 +0800

    feat: add E2E skeleton for TruthFiles (Issue #512)
    Closes #512

---
+++ packages/studio/e2e/truth-files-512.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+
+// E2E skeleton: TruthFiles
+// Issue #512
+// TODO: fill real E2E code in review/test task
+
+test.describe("TruthFiles E2E", () => {
+  test("1. List renders", async ({ page }) => {
+    test.skip();
+  })
+
+  test("2. Search filter", async ({ page }) => {
+    test.skip();
+  })
+
+  test("3. View/edit", async ({ page }) => {
+    test.skip();
+  })
+
+  test("4. Create new", async ({ page }) => {
+    test.skip();
+  })
+});
