7caad7c110ea112a5072f7afd4b7590532a4ca1d
zhenhuaxi <qlc001@qilincl.top>
2026-07-08 15:03:30 +0800

    feat: add E2E skeleton for LogViewer (Issue #508)
    Closes #508

---
+++ packages/studio/e2e/log-viewer-508.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+
+// E2E skeleton: LogViewer
+// Issue #508
+// TODO: fill real E2E code in review/test task
+
+test.describe("LogViewer E2E", () => {
+  test("1. List renders", async ({ page }) => {
+    test.skip();
+  })
+
+  test("2. Level filter", async ({ page }) => {
+    test.skip();
+  })
+
+  test("3. Search", async ({ page }) => {
+    test.skip();
+  })
+
+  test("4. Refresh", async ({ page }) => {
+    test.skip();
+  })
+});
