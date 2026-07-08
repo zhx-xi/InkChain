2547b2305308515aeaa679e438791e652cf15fc9
zhenhuaxi <qlc001@qilincl.top>
2026-07-08 15:03:39 +0800

    feat: add E2E skeleton for ServiceListPage (Issue #511)
    Closes #511

---
+++ packages/studio/e2e/service-list-page-511.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+
+// E2E skeleton: ServiceListPage
+// Issue #511
+// TODO: fill real E2E code in review/test task
+
+test.describe("ServiceListPage E2E", () => {
+  test("1. List renders", async ({ page }) => {
+    test.skip();
+  })
+
+  test("2. Add/edit", async ({ page }) => {
+    test.skip();
+  })
+
+  test("3. Connection test", async ({ page }) => {
+    test.skip();
+  })
+
+  test("4. Delete", async ({ page }) => {
+    test.skip();
+  })
+});
