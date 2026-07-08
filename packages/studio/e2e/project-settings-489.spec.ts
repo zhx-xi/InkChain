e726b9ea1a5c7c176c6e68d7673497f010ee98f0
zhenhuaxi <qlc001@qilincl.top>
2026-07-08 15:02:07 +0800

    feat: add E2E skeleton for ProjectSettings (Issue #489)
    Closes #489

---
+++ packages/studio/e2e/project-settings-489.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+// E2E skeleton: ProjectSettings
+// Issue #489
+// TODO: fill real E2E code in review/test task
+
+test.describe("ProjectSettings E2E", () => {
+  test("1. Tab renders", async ({ page }) => {
+    test.skip();
+  })
+
+  test("2. Agent cards", async ({ page }) => {
+    test.skip();
+  })
+
+  test("3. Save action", async ({ page }) => {
+    test.skip();
+  })
+
+  test("4. Skill list", async ({ page }) => {
+    test.skip();
+  })
+
+  test("5. Preset switching", async ({ page }) => {
+    test.skip();
+  })
+});
