aaf57fc986911582add285916016ded8d5d03b2f
zhenhuaxi <qlc001@qilincl.top>
2026-07-08 15:01:13 +0800

    feat: add E2E skeleton for DaemonControl (Issue #482)
    Closes #482

---
+++ packages/studio/e2e/daemon-control-482.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+// E2E skeleton: DaemonControl
+// Issue #482
+// TODO: fill real E2E code in review/test task
+
+test.describe("DaemonControl E2E", () => {
+  test("1. Page renders", async ({ page }) => {
+    test.skip();
+  })
+
+  test("2. Start/stop", async ({ page }) => {
+    test.skip();
+  })
+
+  test("3. Status display", async ({ page }) => {
+    test.skip();
+  })
+});
