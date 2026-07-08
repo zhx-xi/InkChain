67ed4ba52c23188068d346563850df1918b9b337
zhenhuaxi <qlc001@qilincl.top>
2026-07-08 15:02:58 +0800

    feat: add E2E skeleton for EditDashboard (Issue #504)
    Closes #504

---
+++ packages/studio/e2e/edit-dashboard-504.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+
+// E2E skeleton: EditDashboard
+// Issue #504
+// TODO: fill real E2E code in review/test task
+
+test.describe("EditDashboard E2E", () => {
+  test("1. Panel renders", async ({ page }) => {
+    test.skip();
+  })
+
+  test("2. KPI display", async ({ page }) => {
+    test.skip();
+  })
+
+  test("3. Module collapse", async ({ page }) => {
+    test.skip();
+  })
+
+  test("4. Data refresh", async ({ page }) => {
+    test.skip();
+  })
+});
