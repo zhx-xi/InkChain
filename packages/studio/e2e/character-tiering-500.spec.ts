ef5dd3d8fe6421eea277085bc68c327c5df16152
zhenhuaxi <qlc001@qilincl.top>
2026-07-08 15:02:47 +0800

    feat: add E2E skeleton for CharacterTiering (Issue #500)
    Closes #500

---
+++ packages/studio/e2e/character-tiering-500.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+// E2E skeleton: CharacterTiering
+// Issue #500
+// TODO: fill real E2E code in review/test task
+
+test.describe("CharacterTiering E2E", () => {
+  test("1. Tab switching", async ({ page }) => {
+    test.skip();
+  })
+
+  test("2. Card list display", async ({ page }) => {
+    test.skip();
+  })
+
+  test("3. Search filter", async ({ page }) => {
+    test.skip();
+  })
+
+  test("4. Create new character", async ({ page }) => {
+    test.skip();
+  })
+
+  test("5. Drag reorder", async ({ page }) => {
+    test.skip();
+  })
+});
